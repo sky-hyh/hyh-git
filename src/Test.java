d98e61a59609ac8f34ae141a80aa8062e2f3d493
郝永恒 <haoyongheng@souche.com>
2018-07-17 09:06:19 +0800

    1 master

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,9 @@
 public class Test {
     public static void main(String[] args) {
         int a = 100;
-        int b = 200;
+        int b = 100;
     }
-    public void test(){
-        String s = "abc";
+    public void master(){
+        String s = "master";
     }
 }
